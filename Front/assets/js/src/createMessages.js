f3c2131e5b5b559c10a0e1b2197b3ca9809f944b
Alana <alanac248@gmail.com>
2024-10-03 11:38:14 -0300

    Arrumando detalhes

--- Front/assets/js/src/createMessages.js
+++ Front/assets/js/src/createMessages.js
@@ -1,5 +1,5 @@
-//criando o post do forum 
-function createPost(nome, mensagem, idMensagem,  nivel) {
+//criando o post do forum
+function createPost(nome, mensagem, idMensagem, nivel) {
   const post = document.createElement("div");
   post.classList.add("post");
   post.setAttribute("data-id", idMensagem);
@@ -20,14 +20,38 @@ function createPost(nome, mensagem, idMensagem,  nivel) {
   <p class="post-text">${mensagem}</p>
 
   <div class="post-btns">
-    <button class="post-btn-like"><i class="bi bi-heart icon-post"></i>Curtir</button>
+    <button class="post-btn-like" id="like-post"><i class="bi bi-heart icon-post"></i>Curtir</button>
   </div>
-
-  <button id="show-comments" class="show-comments-btn"><i class="bi bi-caret-down-fill"></i> Comentários</button>
 
   <div class="comments-container" id="comments-${idMensagem}"></div>
 
              `;
+
+  const btnCurtir = post.querySelector(".post-btn-like");
+  btnCurtir.addEventListener("click", () => {
+
+    if (btnCurtir.innerHTML.includes("Curtido")) {
+
+      btnCurtir.innerHTML = `<i class="bi bi-heart icon-post"></i> Curtir`;
+    } else {
+   
+      btnCurtir.innerHTML = `<i class="bi bi-heart-fill icon-post"></i> Curtido`;
+    }
+  });
+
+  const btnSeguir = post.querySelector(".post-btn-follow");
+  btnSeguir.addEventListener("click", () => {
+   
+    if (btnSeguir.innerHTML.includes("Seguindo")) {
+ 
+      btnSeguir.innerHTML = `Seguir <i class="bi bi-plus-circle icon-post"></i>`;
+    } else {
+  
+      btnSeguir.innerHTML = `Seguindo <i class="bi bi-check-circle icon-post"></i>`;
+    }
+  });
+
+
   return post;
 }
 
@@ -45,4 +69,3 @@ function createComment(nome, comentario) {
   `;
   return comment;
 }
-
--- Front/assets/js/src/trilha.js
+++ Front/assets/js/src/trilha.js
@@ -7,12 +7,12 @@ const trilhas = [
         id_nivel: 1,
         descricao: "Qual a principal razão e motivação da Natura?",
         alternativas: [
-            "Promover o bem-estar",
-            "Aumentar o lucro",
-            "Expandir globalmente",
-            "Reduzir custos"
-          ],
-          correta: "Promover o bem-estar"
+          "Promover o bem-estar",
+          "Aumentar o lucro",
+          "Expandir globalmente",
+          "Reduzir custos",
+        ],
+        correta: "Promover o bem-estar",
       },
       {
         id_nivel: 2,
@@ -21,21 +21,20 @@ const trilhas = [
           "Brasil",
           "Argentina",
           "Chile",
-          "Colômbia"
+          "Colômbia",
         ],
-        correta: "Brasil"
+        correta: "Brasil",
       },
       {
         id_nivel: 3,
-        descricao:
-          "Quais são os níveis que as consultoras podem ter dentro da natura?",
-          alternativas: [
-            "Semente, bronze, metal, ouro e diamante",
-            "Planta, bronze, prata, ouro e diamante",
-            "Semente, bronze, prata, ouro e diamante",
-            "Semente, bronze, prata, ouro e platina"
-          ],
-        correta: "Semente, bronze, prata, ouro e diamante"
+        descricao: "Quais são os níveis que as consultoras podem ter dentro da Natura?",
+        alternativas: [
+          "Semente, bronze, metal, ouro e diamante",
+          "Planta, bronze, prata, ouro e diamante",
+          "Semente, bronze, prata, ouro e diamante",
+          "Semente, bronze, prata, ouro e platina",
+        ],
+        correta: "Semente, bronze, prata, ouro e diamante",
       },
     ],
   },
@@ -45,18 +44,25 @@ const trilhas = [
     niveis: [
       {
         id_nivel: 4,
-        descricao: "Qual o produto mais vendido?",
-        alternativa: "Perfume",
+        descricao: "Qual dessas linhas de produtos NÃO existe?",
+        alternativas: [
+          "Ekos",
+          "Todo dia",
+          "Kaiak",
+          "Papai e criança",
+        ],
+        correta: "Papai e criança",
       },
       {
         id_nivel: 5,
-        descricao: "Qual a principal linha?",
-        alternativa: "Kaiak",
-      },
-      {
-        id_nivel: 6,
-        descricao: "Qual a linha menos vendida?",
-        alternativa: "a",
+        descricao: "Qual dessas linhas tem fragrâncias inspiradas na natureza para um estilo de vida ativo e aventureiro?",
+        alternativas: [
+          "Ekos",
+          "Todo dia",
+          "Kaiak",
+          "Luna",
+        ],
+        correta: "Kaiak",
       },
     ],
   },
@@ -65,37 +71,36 @@ const trilhas = [
     nome: "Vendas",
     niveis: [
       {
-        id_nivel: 7,
+        id_nivel: 6,
         descricao: "Quais tipos de venda a Natura tem?",
         alternativas: [
-          "Venda casada, venda direta e  varejo",
+          "Venda casada, venda direta e varejo",
           "Venda online, venda direta",
-          "Venda online, venda direta e  varejo",
-          "Venda online e  varejo"
+          "Venda online, venda direta e varejo",
+          "Venda online e varejo",
         ],
-        correta: "Venda online, venda direta e  varejo",
+        correta: "Venda online, venda direta e varejo",
       },
       {
-        id_nivel: 8,
+        id_nivel: 7,
         descricao: "Qual o percentual de lucro do nível semente da Natura?",
         alternativas: [
           "30%",
           "20%",
           "15%",
-          "5%"
+          "5%",
         ],
-        correta: "20%"
+        correta: "20%",
       },
       {
-        id_nivel: 9,
-        descricao:
-          "Qual das alternativas a seguir é INCORRETA sobre os requisitos para se tornar uma consultora Natura? ",
-          alternativas: [
-            "Fazer um curso preparatório",
-            "Ser maior de 18 anos",
-            "Realizar o seu primeiro pedido",
-            "Preencher o formulário de cadastro"
-          ],
+        id_nivel: 8,
+        descricao: "Qual das alternativas a seguir é INCORRETA sobre os requisitos para se tornar uma consultora Natura?",
+        alternativas: [
+          "Fazer um curso preparatório",
+          "Ser maior de 18 anos",
+          "Realizar o seu primeiro pedido",
+          "Preencher o formulário de cadastro",
+        ],
         correta: "Fazer um curso preparatório",
       },
     ],
